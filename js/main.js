e8df8b6e67340b9d24575e8fae38cb9a7e28d810
Alexey <93075959+LEXXSAS@users.noreply.github.com>
2022-09-11 18:54:10 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,82 @@
+//var btn = document.querySelector(".btn");
+//
+//btn.addEventListener("click", function() {
+// this.classList.add("active");
+//});
+
+//var btn = document.querySelectorAll(".btn");
+//btn.forEach(function(btnActive) {
+//    btnActive.addEventListener("click", function(e) {
+//        this.classList.add("active");
+//    })
+//})
+
+
+// Получить контейнер с элементами 
+var btnsAll = document.getElementById("operator_btns");
+//Получить все элементы с class="btn" из контейнера operator_btns
+var btns = btnsAll.getElementsByClassName("btn");
+//Выполнить цикл по кнопкам и добавить класс к текущей/нажатой кнопке
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        
+        if (current.length > 0) {
+         current[0].className = current[0].className.replace(" active", "");
+        }
+
+        this.className += " active";
+    });
+}
+
+
+//let btns = document.querySelectorAll(".btn");
+//btns.forEach((e) => {
+//    e.addEventListener("click", () => {
+//            if (e.classList.contains("btn")) {
+//            e.classList.add("disable");
+//            e.classList.remove("active");
+//    } 
+//        
+//})
+//
+//})
+
+
+//let btns = document.querySelectorAll(".btn");
+//function abra {
+//   btns.forEach(e => {
+//  e.classList.remove('active');
+//}); 
+//};
+
+
+
+
+
+
+//
+//var btnAll = document.querySelectorAll(".btn");
+//
+//function allDisable() {
+//
+//}
+//allDisable();
+
+//var btnsAll = document.querySelectorAll(".btn");
+//btnsAll.addEventListener("click", function() {
+//    
+//})
+
+
+//var btn = document.querySelector(".btn");
+//
+//btn.addEventListener("click", function() {
+// event.target.classList.add("active");
+//});
+
+
+
 var op;
 var array = [];
 
@@ -54,5 +133,7 @@ enter.addEventListener('click', arrayConsole);
 
 function arrayConsole() {
 //    console.log(array);
+    
     document.getElementById("result_two").innerHTML =  array.join('; ');
 }
+
